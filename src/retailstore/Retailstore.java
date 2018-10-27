f85e551cff996407835091b9a36c49e1e6c11126
saimahithanatakala <40490121+saimahithanatakala@users.noreply.github.com>
2018-10-27 20:02:47 +0530

    Add files via upload

---
+++ src/retailstore/Retailstore.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package retailstore;
+
+
+import my.retailmain.*;
+/**
+ *
+ * @author Intel
+ */
+public class Retailstore {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        retailmain r1=new retailmain(new javax.swing.JFrame(),true);
+        // TODO code application logic here
+    }
+    
+}
